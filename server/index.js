5315d4068001d1b9c141fe744b83be7a4913d2a2
trevormedley <trevormedley10@gmail.com>
2021-11-18 15:16:27 -0500

    Added rollbar

--- server/index.js
+++ server/index.js
@@ -1,3 +1,14 @@
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '7bf2ec72f4a74bfb85fe85f2ad0e2849',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 require('dotenv').config();
 const express = require('express');
 const app = express();
